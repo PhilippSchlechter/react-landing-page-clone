9331da2a52699ff7beb5fbc85a6aaa6ae67d2fbb
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-19 20:50:28 +0200

    Add first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
 
-function App() {
+import { css } from '@emotion/react';
+import logo from './images/logo.svg';
+
+const mainStyles = css`
+  background: #f3f5ff;
+`;
+const headerStyles = css`
+  display: flex;
+  justify-content: space-between;
+`;
+const navStyles = css``;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div css={mainStyles}>
+      <header css={headerStyles}>
+        <img src={logo} width="80" height="60" alt="logo planzerio" />
+        <nav css={navStyles}>
+          <a href="#aaa">Pricing</a>
+          <a href="#aaa">Features</a>
+          <a href="#aaa">Use Cases</a>
+        </nav>
+        <div>
+          <a href="#aaa">Sign in</a>
+          <a href="#aaa">Get started</a>
+        </div>
       </header>
     </div>
   );
 }
-
-export default App;
